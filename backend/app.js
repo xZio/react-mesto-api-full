83ba5ef5a138f3bfa54a16f26d3d5a05ee480057
Alexander Botanov <sky_11@inbox.ru>
2022-08-30 14:52:38 +0300

    PR15: 1st review

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
 const cors = require('cors');
+const helmet = require('helmet');
 const routes = require('./routes');
 const validationErrors = require('./middlewares/errors');
 
@@ -15,6 +16,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb');
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.use(routes);
 app.use(errors());
